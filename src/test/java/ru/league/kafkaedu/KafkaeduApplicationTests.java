b16c7f97579f173be0d57221205dc88e252c4bb7
ntolmachev <40675616+Guzime@users.noreply.github.com>
2023-07-24 17:15:49 +0300

    init

---
+++ src/test/java/ru/league/kafkaedu/KafkaeduApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.league.kafkaedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaeduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
